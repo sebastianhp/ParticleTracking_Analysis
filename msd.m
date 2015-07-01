831f89845cd2399dbc8d91bc20ffae96ec5fb97c
sebastianhp <sebastianh25@gmail.com>
2015-06-30 23:24:56 -0400

    Fixed line so that every splitTrack is now properly rezeroed. Also added descriptive comments.

--- msd.m
+++ msd.m
@@ -1,30 +1,50 @@
-% MSD: calculates mean square displacement from an ensemble of trajectory 
-% data of single particles.
+% MSD 
+% Calculates mean square displacement from an ensemble of trajectory data 
+% of single particles.
 %
 % List of inputs:
 %
 % * tracksCell (cell array): 1xnumTracks cell array containing a single
-% * trajectory in each cell
-% * divs (int): number of times each trajectory will be divided
-% * divsize (int): how many timeframes for each division
-% * dim (int): dimension along which to take msd. 1 is along x, 2 along y
-% 
+%   trajectory in each cell
+% * divs (int): Number of times each trajectory will be divided. This 
+%               variable defaults to 1, i.e. uses the entirety of the 
+%               trajectory
+% * divsize (int): How many timeframes for each division. Defaults to the
+%                   number of timesteps of the most brief trajectory in 
+%                   tracksCell.
+% * dim (int): Dimension along which to take msd. 1 is along x, 2 along y.
+%               Defaults to msd along x.
+%
+% Format of tracked files
 % The program expects the tracked files to be in the format:
-% Column 1: Position in x
-% Column 2: Position in y
-% Column 3: Timestamp
-% Each row its own timestep
+% 
+% * Column 1: Position in x
+% * Column 2: Position in y
+% * Column 3: Timestamp
+% * Each row is its own timestep
 
+function [times, msd] = msd(varargin)
 
-function [times, msd] = msd(tracksCell, divs, divsize, dim)
-
+tracksCell = {};
+divs = 1;
+divsize = -1;
+dim = 1;
+for i=1:2:length(varargin)
+    switch varargin{i}
+        case 'tracksCell'
+            tracksCell = varargin{i+1};
+        case 'divs'
+            divs = varargin{i+1};
+        case 'divsize'
+            divsize = varargin{i+1};
+        case 'dim'
+            dim = varargin{i+1};
+    end
+end
+            
 marker = false;
 sz = size(tracksCell);
 numTracks = sz(2);
-
-if divs == -1
-    divs = 1;
-end
 
 splitTracksCell = cell(numTracks,divs+1); % Rows are different runs
                                         % Columns are different divisions
@@ -43,7 +63,7 @@ for i=1:numTracks
     end
     trackedRun = tracksCell{i};
     for j=1:divs
-        temp = trackedRun( (1+(j-1)*divsize):j*divsize,dim) - trackedRun(1,dim);
+        temp = trackedRun( (1+(j-1)*divsize):j*divsize,dim) - trackedRun((1+(j-1)*divsize),dim);
         splitTracksCell{i,j} = temp;
     end
     splitTracksCell{i,divs+1} = trackedRun(1:divsize,3) - trackedRun(1,3); % time column
@@ -80,5 +100,6 @@ end
 temp = splitTracksCell{1,divs+1};
 times = temp(1:divsize);
 
+clear temp tracksCell divs divsize dim marker sz numTracks splitTracksCell sizes 
+
 end
-        
